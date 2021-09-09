642e63ac30fece7510b7f2327d25a27369f38bea
MichaelStafford <michaelstafford808@gmail.com>
2021-09-09 13:09:47 -1000

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -5,11 +5,21 @@ describe('shout(string)', function() {
   })
 })
 
+function shout(string) {
+return string.toUpperCase(); 
+} 
+
+
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello');
   })
 })
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
 
 describe('logShout(string)', function() {
   it('takes a string argument and logs it in all caps using console.log()', function() {
@@ -23,6 +33,11 @@ describe('logShout(string)', function() {
   })
 })
 
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+
 describe('logWhisper(string)', function() {
   it('takes a string argument and logs it in all lowercase using console.log()', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough();
@@ -34,17 +49,39 @@ describe('logWhisper(string)', function() {
     console.log.restore();
   })
 })
-
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
 describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!");
   })
 
+
+
   it('returns "YES INDEED!" if `string` is uppercase', function() {
     expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!");
   })
+
+
 
   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.");
   })
 })
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+}
